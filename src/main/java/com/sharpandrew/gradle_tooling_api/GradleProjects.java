5a01b048339435e0dbdfe66e2df63ce43d9aefc1
Andrew Sharp <andrew.sharp57@gmail.com>
2017-03-25 22:13:23 -0400

    main method

--- src/main/java/com/sharpandrew/gradle_tooling_api/GradleProjects.java
+++ src/main/java/com/sharpandrew/gradle_tooling_api/GradleProjects.java
@@ -1,11 +1,18 @@
 package com.sharpandrew.gradle_tooling_api;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 import org.gradle.tooling.model.GradleProject;
 
 public class GradleProjects {
+    public static void main(String[] args) {
+        Path pathToGradleProject = Paths.get(args[0]);
+        Path jvmTmpDir = Paths.get(args[1]);
+        System.out.println(getGradleProject(pathToGradleProject, jvmTmpDir));
+    }
+
     public static GradleProject getGradleProject(Path pathToGradleProject, Path jvmTmpDir) {
         ProjectConnection projectConnection = GradleConnector.newConnector()
                 .forProjectDirectory(pathToGradleProject.toFile())
