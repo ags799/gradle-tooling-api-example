b1ef303b320cb53af9db53207df0bf5e5e67c801
Andrew Sharp <andrew.sharp57@gmail.com>
2017-03-25 21:56:52 -0400

    add jvmTmpDir argument

--- src/main/java/com/sharpandrew/gradle_tooling_api/GradleProjects.java
+++ src/main/java/com/sharpandrew/gradle_tooling_api/GradleProjects.java
@@ -6,12 +6,13 @@ import org.gradle.tooling.ProjectConnection;
 import org.gradle.tooling.model.GradleProject;
 
 public class GradleProjects {
-    public static GradleProject getGradleProject(Path pathToGradleProject) {
+    public static GradleProject getGradleProject(Path pathToGradleProject, Path jvmTmpDir) {
         ProjectConnection projectConnection = GradleConnector.newConnector()
                 .forProjectDirectory(pathToGradleProject.toFile())
                 .connect();
         try {
-            return projectConnection.model(GradleProject.class).get();
+            String jvmTmpDirArgument = String.format("-Djava.io.tmpdir=%s", jvmTmpDir);
+            return projectConnection.model(GradleProject.class).setJvmArguments(jvmTmpDirArgument).get();
         } finally {
             projectConnection.close();
         }
